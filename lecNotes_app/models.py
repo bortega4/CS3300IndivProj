dec07d138d9c85ad64c304e664fcf9c3404cff0a
Bryant Ortega <bortega4@uccs.edu>
2024-04-06 23:05:27 -0600

    this is v1.2

--- lecNotes_app/models.py
+++ lecNotes_app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+#Remember that python goes line by line
+# If issues arise, you may have to rearrange models
+# Refer to PortfolioApp
+
+class Course(models.Model):
+    coursePrefix = models.CharField(max_length=200)
+    courseCode = models.IntegerField()
+    courseName = models.CharField(max_length=200, blank=True)
+    #decided to make lecturer a required field
+    courseLecturer = models.CharField(max_length=200)
+
+#define default string to return the name for representing the Model object
+def __str__(self):
+    #will think of a way to identify a course...
+    #maybe make prefix and code a single field?
+    #require course name?
+    return self.coursePrefix
+
+#Return the URL to access a particular instance of MyModelName.
+#if you define this method then Django will automatically
+#add a "View on Site" button to the model's record editing screens in Admin Site
+def get_absolute_url(self):
+    return reverse('course-detail', args=[str(self.id)])
+
+
+
+
