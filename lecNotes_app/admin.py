b45ff74f5a8a455007c46c7cb8d852b1d0821c1d
Bryant Ortega <bortega4@uccs.edu>
2024-04-07 16:00:45 -0600

    this is v1.3

--- lecNotes_app/admin.py
+++ lecNotes_app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Course
+from .models import Course, Lecturer, LectureNotes
 
 admin.site.register(Course)
+admin.site.register(Lecturer)
+admin.site.register(LectureNotes)
--- lecNotes_app/models.py
+++ lecNotes_app/models.py
@@ -6,25 +6,69 @@ from django.urls import reverse
 # If issues arise, you may have to rearrange models
 # Refer to PortfolioApp
 
+#blank = True makes a field not required
 class Course(models.Model):
     coursePrefix = models.CharField(max_length=200)
     courseCode = models.IntegerField()
     courseName = models.CharField(max_length=200, blank=True)
     #decided to make lecturer a required field
     courseLecturer = models.CharField(max_length=200)
+    
+    '''
+    will try putting courseCode and prefix together
+    courseID = coursePrefix + courseCode
+    '''
 
-#define default string to return the name for representing the Model object
-def __str__(self):
-    #will think of a way to identify a course...
-    #maybe make prefix and code a single field?
-    #require course name?
-    return self.coursePrefix
+    #define default string to return the name for representing the Model object
+    def __str__(self):
+        #will think of a way to identify a course...
+        #maybe make prefix and code a single field?
+        #require course name?
+        return self.coursePrefix
 
-#Return the URL to access a particular instance of MyModelName.
-#if you define this method then Django will automatically
-#add a "View on Site" button to the model's record editing screens in Admin Site
-def get_absolute_url(self):
-    return reverse('course-detail', args=[str(self.id)])
+    #Return the URL to access a particular instance of MyModelName.
+    #if you define this method then Django will automatically
+    #add a "View on Site" button to the model's record editing screens in Admin Site
+    def get_absolute_url(self):
+        return reverse('course-detail', args=[str(self.id)])
+    
+    #course can have multiple lecturers, established in lecturer model
+    #multiple lecture notes, relationship established in LectureNotes model
+    
+
+class Lecturer(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+    
+    #lecturer can have multiple courses, many lecturers to many courses?
+    courses = models.ManyToManyField(Course)
+    
+
+class LectureNotes(models.Model):
+    coursePrefix = models.CharField(max_length=200)
+    courseCode = models.IntegerField()
+    courseName = models.CharField(max_length=200, blank = True)
+    lectureDate = models.DateField()
+    lectureNum = models.IntegerField()
+    notesAuthor = models.CharField(max_length=200, blank = True)
+    #lectureDocument will go here
+
+    #courseLecturer, may have to come back and play w this field
+    courseLecturer = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.lectureNum
+    def get_absolute_url(self):
+        return reverse('lecture-detail', args=[str(self.id)])
+    
+    #lectureNotes belong to one class, many lectureNotes to one course
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, default=None)
+
+    
+    
+
 
 
 
